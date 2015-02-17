92e491bdedded587fa79afb416cd7336fd5321a9
terimiyaki <wackoli123@yahoo.com>
2015-02-17 00:11:36 -0500

    added some javascript for hiding and showing pieces of the DOM

---
+++ app.js
@@ -0,0 +1,78 @@
+var game = {
+	answer : 0,
+	totalGuesses : 0,
+	agent : "dog"
+};
+
+game.initialize = function(){
+	this.answer = generateRandomNumber(100);
+	this.guesses = [];
+	this.notification = createNotification();
+	$("#gameScreen").removeClass("hide");
+};
+
+game.setTotalGuesses = function(num){
+	this.totalGuesses = num;
+	$("#selectMode").addClass("hide");
+	$("#selectAgent").removeClass("hide");
+};
+
+game.setAgent = function(name){
+	this.agent = name;
+	$("#selectAgent").addClass("hide");
+	this.initialize();
+};
+
+game.checkAnswer = function(){
+	console.log("I am here");
+	var input = $("input").val();
+	if(!isNaN(input)){
+		if(this.guesses.indexOf(input) === -1){
+			this.guesses.push(input);
+			if(input === this.answer){
+				console.log("you win");
+			} else if (input > this.answer){
+
+			}
+		}
+	} else {
+
+	}
+};
+
+game.getAHint = function(){
+
+};
+
+var generateRandomNumber = function(upperBound){
+	return Math.ceil(Math.random() * upperBound);
+};
+
+function createNotification(){
+	var notification = {
+		text : "",
+		mood : "",
+	};
+
+	notification.change = function(textIn, moodIn){
+		this.text = textIn;
+		this.mood = moodIn;
+		this.render();
+	};
+
+	notification.render = function(){
+		$(".notification").text(this.text);
+		if($(".notification").hasClass("warm") && this.mood !== "warm"){
+			$(".notification").removeClass("warm");
+			$(".notification").addClass(this.mood);
+		} else if($(".notification").hasClass("cold") && this.mood !== "cold") {
+			$(".notification").removeClass("cold");
+			$(".notification").addClass(this.mood);
+		} else if($(".notification").hasClass("neutral") && this.mood !== "neutral"){
+			$(".notification").removeClass("neutral");
+			$(".notification").addClass(this.mood);
+		}
+	};
+
+	return notification;
+}
